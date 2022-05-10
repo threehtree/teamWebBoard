009d34144bc7afbeebd2ab2740bc073f65ae3c92
LTH4823 <77533862+LTH4823@users.noreply.github.com>
2022-05-10 11:32:21 +0900

    add to team project db vo & dto setting

--- src/main/java/org/zerock/teamwebboard/domain/Contract.java
+++ src/main/java/org/zerock/teamwebboard/domain/Contract.java
@@ -11,7 +11,20 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class Contract {
 
-
+    private Integer contractNum;
+    private Integer categoryNum;
+    private String requesterID;
+    private String contractorID;
+    private String area;
+    private String workScope;
+    private String workDetail;
+    private Integer workLimit;
+    private String contractState;
+    private String contractName;
+    private String contractFile;
+    private String priceState;
+    private String negotiationState;
+    private String declarationState;
 
     private LocalDate regDate;
     private LocalDate updateDate;
--- src/main/java/org/zerock/teamwebboard/dto/ContractDTO.java
+++ src/main/java/org/zerock/teamwebboard/dto/ContractDTO.java
@@ -2,6 +2,26 @@ package org.zerock.teamwebboard.dto;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class ContractDTO {
+
+    private Integer contractNum;
+    private Integer categoryNum;
+    private String requesterID;
+    private String contractorID;
+    private String area;
+    private String workScope;
+    private String workDetail;
+    private Integer workLimit;
+    private String contractState;
+    private String contractName;
+    private String contractFile;
+    private String priceState;
+    private String negotiationState;
+    private String declarationState;
+
+    private LocalDate regDate;
+    private LocalDate updateDate;
 }
